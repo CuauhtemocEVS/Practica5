92cb70a8757b70bbb4a3ae74158fde6c95b476b1
TemoVas <160094244+CuauhtemocEVS@users.noreply.github.com>
2026-02-25 20:11:50 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/itson/prctica5/detalle_pelicula.kt
@@ -0,0 +1,41 @@
+package mx.edu.itson.prctica5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class detalle_pelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalle_pelicula)
+
+        val iv_pelicula_imagen: ImageView = findViewById(R.id.iv_pelicula_imagen)
+        val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
+        val tv_nombre_desc: TextView = findViewById(R.id.tv_nombre_desc)
+
+        val bundle = intent.extras
+        if(bundle != null){
+            iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
+            tv_nombre_pelicula.setText(bundle.getString("titulo"))
+            tv_nombre_desc.setText(bundle.getString("sinopsis"))
+        }
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
